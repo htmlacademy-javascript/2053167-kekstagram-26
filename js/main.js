99fbd6dc16d9570bfe3e2c87cd2574d04299cd71
Juli <julimouchework@gmail.com>
2022-06-12 18:54:03 +0300

    подключила js. написала первые функции.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getLengthString(stringNumber, maxLength ) {
+
+  let maxStringLength = 140;
+  return (maxLength < maxStringLength);
+}
