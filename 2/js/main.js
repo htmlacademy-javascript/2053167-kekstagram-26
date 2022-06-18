c662c17f4439666612f7bd427ca886fb37698548
Keks <keks@htmlacademy.ru>
2022-06-18 12:44:44 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,11 @@
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+getRandomInteger ();
+function getLengthString(stringNumber, maxLength ) {
+  let maxStringLength = 140;
+
+  return (maxLength < maxStringLength);
+}
+getLengthString();
