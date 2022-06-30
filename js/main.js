2a27b4d174c370afe9bb08780ef99f307168f3d6
Juli <julimouchework@gmail.com>
2022-07-01 00:08:20 +0300

    Удаление комментариев

--- js/main.js
+++ js/main.js
@@ -1,27 +1,12 @@
-//возвращающая случайное целое число из переданного диапазона включительно:
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
-//для проверки максимальной длины строки
 function checkStringLength (string, length) {
   return string.length <= length;
 };
-/*
-Структура каждого объекта должна быть следующей:
-(id, url, description, likes, comments)
-id, число — идентификатор описания. Это число от 1 до 25. Идентификаторы не должны повторяться.
-url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25.
-Адреса картинок не должны повторяться.
-description, строка — описание фотографии. Описание придумайте самостоятельно.
-likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
- Количество комментариев к каждой фотографии вы определяете на своё усмотрение.
- Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
-*/
-
 const createDescription = [
   'Good',
   'Not good',
@@ -51,7 +36,6 @@ const commentsFoto = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 const similarUsersCount = 25;
-
 const createComments = () => {
   return {
     id: getRandomPositiveInteger(0,1000),
@@ -69,6 +53,5 @@ const createUsers = () => {
     comments: createComments (),
   };
 };
-
 const similarUsers = Array.from({length: similarUsersCount}, createUsers);
 console.log(createUsers);
