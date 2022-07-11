1fb8cfbb396c49d19cf395a68d724040d9e22e4d
Keks <keks@htmlacademy.ru>
2022-07-11 17:34:17 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/picture.js
@@ -0,0 +1,10 @@
+/* На основе временных данных для разработки и шаблона #picture
+ создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
+
+Адрес изображения url подставьте как атрибут src изображения.
+Количество лайков likes выведите в блок .picture__likes.
+Количество комментариев comments выведите в блок .picture__comments.
+Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
+*/
+const NEW_PICTURE = document.querySelector('#picture');
+//NEW_PICTURE.classList.remove('hidden');
